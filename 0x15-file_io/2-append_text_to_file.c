4fe29f56aa3a8db904e4750cd57fe6465e213c3e
MoaazOmar <moaaz20550@gmail.com>
2023-08-11 18:28:02 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,36 +9,36 @@
 *         if the function doesn't exist- -1.
 *         Otherwise - 1.
 */
-nt append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+    int o, w, len = 0;
 
-	if (filename == NULL)
-	{
-		return -1;
-	}
+    if (filename == NULL)
+    {
+        return (-1);
+    }
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len] != '\0'; len++)
-		{
-			;
-		}
-	}
+    if (text_content != NULL)
+    {
+        for (len = 0; text_content[len] != '\0'; len++)
+        {
+            ;
+        }
+    }
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	if (o == -1)
-	{
-		return -1;
-	}
+    o = open(filename, O_WRONLY | O_APPEND);
+    if (o == -1)
+    {
+        return (-1);
+    }
 
-	w = write(o, text_content, len);
-	if (w == -1)
-	{
-		close(o);
-		return -1;
-	}
+    w = write(o, text_content, len);
+    if (w == -1)
+    {
+        close(o);
+        return (-1);
+    }
 
-	close(o);
-	return 1;
+    close(o);
+    return (1);
 }
